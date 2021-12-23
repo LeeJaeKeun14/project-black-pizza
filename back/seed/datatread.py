a92238dba57b4a286a1cdb53ecc0f05ec28c059b
[3기-2반] 이무원 <tlsehd482@gmail.com>
2021-12-23 23:25:13 +0900

    feat:seed 수정

--- back/seed/datatread.py
+++ back/seed/datatread.py
@@ -1,23 +1,23 @@
 import pandas as pd
 
 # df = pd.read_csv('/Users/whitewind/Desktop/elis/blackpizza/back/seed/temp_movie_data.csv')
-df = pd.read_csv('./seed/temp_movie_data.csv')
+df = pd.read_csv('./temp_movie_data.csv')
 # print(df.head())
 
 def seed_contents():
     contents = []
-    for line in df:
-        print(line)
-        # content = {}
-        # content["title"] = line['제목']
-        # content["orgin_title"] = line['원제']
-        # content["open"] = line['개봉일']
-        # content["score"] = line['평점']
-        # content["runtime"] = line['재생 시간']
-        # content["director"] = line['감독']
-        # content["synopsis"] = line['시놉시스']
-        # content["image"] = line['이미지']
-        # contents.append(content)
+    for i, line in df.iterrows():
+        # print(line)
+        content = {}
+        content["title"] = line['제목']
+        content["orgin_title"] = line['원제']
+        content["open"] = line['개봉일']
+        content["score"] = line['평점'].split(" ")
+        content["runtime"] = line['재생 시간']
+        content["director"] = line['감독']
+        content["synopsis"] = line['시놉시스']
+        content["image"] = line['이미지']
+        contents.append(content)
         break
     return contents
 
