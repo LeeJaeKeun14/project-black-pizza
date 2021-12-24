fa664cbcf6a62d4505d1c717e09173392814924e
[3기-2반] 이무원 <tlsehd482@gmail.com>
2021-12-25 00:31:10 +0900

    feat: recommend api 구현

--- back/api/contents.py
+++ back/api/contents.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, Blueprint, request, session
-from models import User, Contents, Genre, Actor
+# from back.models import Buy, Streaming, Rent
+from models import User, Contents, Genre, Actor, Buy, Streaming, Rent
 from db_connect import db
 from datateam.Recommendation import recommendations
 
@@ -41,22 +42,61 @@ def list():
 
 @contents.route('/recommend', methods=['GET', 'POST'])
 def recommend():
-    user_pick = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    if request.method == 'POST':
+        params = request.get_json()
+        user_pick_list = params['data']
+        user_pick_id = [i[0] for i in user_pick_list]
+    else:
+        user_pick_id = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
+    con_list = Contents.query.filter(Contents.id.in_(user_pick_id))
+    user_pick = [i.title for i in con_list]
+    rcm_df = recommendations(user_pick)
+    rcm_title = [line['제목'] for i, line in rcm_df.iterrows()]
+    rcm_list = Contents.query.filter(Contents.title.in_(rcm_title))
+    res = []
+    content = {}
+    for i in rcm_list:
+        ott_info = {}
+        content[i.id] = [i.title, i.image, ott_info]
+        streaming_list = Streaming.query.filter(Streaming.contents_id == i.id)
+        buy_list = Buy.query.filter(Buy.contents_id == i.id)
+        rent_list = Rent.query.filter(Rent.contents_id == i.id)
+        ott_info['streaming'] = [{'ott': i.ott, 'price': i.price, 'quality': i.quality} for i in streaming_list]
+        ott_info['buy'] = [{'ott': i.ott, 'price': i.price, 'quality': i.quality} for i in buy_list]
+        ott_info['rent'] = [{'ott': i.ott, 'price': i.price, 'quality': i.quality} for i in rent_list]
+    
+    ott_count = {}
+    for value in content.values():
+        otts = value[2]
+        for key in otts.keys():
+            for ott_dict in otts[key]:
+                if ott_dict['ott'] in ott_count:
+                    ott_count[ott_dict['ott']] += 1
+                else:
+                    ott_count[ott_dict['ott']] = 0
+
+    res.append(content)
+    res.append(ott_count)
+    print(res)
+    return jsonify(res)
+
+    # user_pick = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     # contents_all = db.session.query(Contents.id, Contents.title, Contents.score, Contents.director).all()
     # actors = db.session.query(Actor.contents_id, Actor.actor).all()
     # genre = db.session.query(Genre.contents_id, Genre.genre).all()
-    contents_all = db.session.query(Contents.id, Contents.title, Contents.score, Contents.director).limit(10)
-    actors = db.session.query(Actor.contents_id, Actor.actor).limit(10)
-    genre = db.session.query(Genre.contents_id, Genre.genre).limit(10)
-    rcm = recommendations(user_pick, contents_all, actors, genre)
-    print(rcm)
-    
+    # contents_all = db.session.query(Contents.id, Contents.title, Contents.score, Contents.director).limit(10)
+    # actors = db.session.query(Actor.contents_id, Actor.actor).limit(10)
+    # genre = db.session.query(Genre.contents_id, Genre.genre).limit(10)
+    # rcm = recommendations(user_pick, contents_all, actors, genre)
+    # print(rcm)
+    # return jsonify(rcm)
+
     # if request.method == 'GET':
     #     recommend_contents = temp_list
     # else:
     #     recommend_contents = temp_list
     # res = {"status": 200, "result": "success", "contents": recommend_contents}
-    return jsonify(rcm)
+    
     # return recommend_contents
 
 
--- back/datateam/Recommendation.py
+++ back/datateam/Recommendation.py
@@ -2,58 +2,94 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-def df_maker(contents_all, actors, genre):
-    df = pd.DataFrame(contents_all)
-    df_actors = pd.DataFrame(actors)
-    df_genre = pd.DataFrame(genre)
-    df.rename(columns={0:'id', 1:'제목', 2:'가중치 평점', 3:'특징'}, inplace = True)
-    df_actors.rename(columns={0:'contents_id', 1:'actor'}, inplace = True)
-    df_genre.rename(columns={0:'contents_id', 1:'genre'}, inplace = True)
-    df['특징'] = df['특징'].apply(lambda x: x.replace(" ", "_") if pd.notnull(x) else x)
-    
-    for i, line in df_actors.iterrows():
-        contents_id = line['contents_id']
-        actor = line['actor']
-        df[df['id'] == contents_id]['특징'] = df[df['id'] == contents_id]['특징'].apply(lambda x: (x+" "+actor.replace(" ", "_")))
-        # df[df['id'] == contents_id]['특징'] = df[df['id'] == contents_id]['특징'][0] + " " +actor.replace(" ", "_")
-        # print(actor.replace(" ", "_"))
-    for i, line in df_genre.iterrows():
-        contents_id = line['contents_id']
-        genre = line['genre']
-        cur = df[df['id'] == contents_id]['특징'][0]
-        df[df['id'] == contents_id]['특징'] = cur + " "+genre
-        # df[df['id'] == contents_id]['특징'] += " " + genre
-        # print(cur)
+## 데이터 읽기
 
-    print(df)
+def read_df():
+    df = pd.read_csv("./seed/temp_movie_data.csv")
+
+    df["장르"][df["장르"].isna()] = "['']"  # 이후에는 제거
+    df["장르"] = df["장르"].apply(lambda x: eval(x))
+    df["장르"] = df["장르"].apply(lambda x : (' ').join(x))
+
+    isna_index = df["평점"].isna()
+    df["평점"][isna_index] = "['']"
+    df["평점"] = df["평점"].apply(lambda x : eval(x))
+
+    def print_ls_1(x):
+        if len(x) == 1:
+            return 0
+        else:
+            return x[1].split()[0]
+        
+    def print_ls_2(x):
+        if len(x) == 1:
+            return "0k"
+        else:
+            try:
+                return x[1].split()[1].strip("()")
+            except:
+                return "0k"
+            
+    df["평가수"] = df["평점"].apply(print_ls_2)
+    df["평점"] = df["평점"].apply(print_ls_1)
+    df["평가수"] = df["평가수"].apply(lambda x: str(int(x[:-1]) * 1000) if x[-1] == "m" else x[:-1])
+    df["평점"] = df["평점"].astype("float")
+    df["평가수"] = df["평가수"].astype("int")
+
+    C = df["평점"].mean()
+    m = df["평가수"].quantile(0.6)
+
+    def weighted_vote_average(record):
+        v = record["평가수"]
+        R = record["평점"]
+        
+        return ((v/(v+m)) * R) + ((m/(m+v)) * C)
+    df["가중치평점"] = df.apply(weighted_vote_average, axis=1)
+
+    not_index = df["감독"].notna()
+    df["감독"][not_index] = df["감독"][not_index].apply(lambda x: x.strip("['']"))
+    df["감독"][not_index] = df["감독"][not_index].apply(lambda x : x.split(" , "))
+
+    def sum_list(x):
+        ls = []
+        for i in x[:5]:
+            ls.append("_".join(i.split(" ")))
+        return " ".join(ls)
+
+    df["감독"][df["감독"].isna()] = "['']"
+    # df["감독"] = df["감독"].apply(lambda x: eval(x))
+    df["감독"] = df["감독"].apply(sum_list)
+
+    df["출연진"][df["출연진"].isna()] = "['']"
+    df["출연진"] = df["출연진"].apply(lambda x: eval(x))
+    df["출연진"] = df["출연진"].apply(sum_list)
+
+    df["특징"] = df["장르"] + " " + df["감독"] + " " + df["출연진"]
     return df
 
+def recommendations(titles, top_n = 20):
 
-def recommendations(user_pick, contents_all, actors, genre, top_n = 20):
-    #DataFrame 만들기('평점', '특징')
-    df = df_maker(contents_all, actors, genre)
-    return df
+    df = read_df()
     # title_list = ["나 홀로 집에", "해리 포터와 마법사의 돌", "쇼생크 탈출"]
+    title_list = titles
+    sum_find = " ".join(list(df[df["제목"].apply(lambda x : x in title_list)]["특징"]))
 
-    # # title_list = titles
-    # sum_find = " ".join(list(df[df["제목"].apply(lambda x : x in title_list)]["특징"]))
+    title_indexes = []
+    for title_name in title_list:
+        title_movie = df[df["제목"] == title_name]
+        title_indexes += list(title_movie.index.values)
 
-    # title_indexes = []
-    # for title_name in title_list:
-    #     title_movie = df[df["제목"] == title_name]
-    #     title_indexes += list(title_movie.index.values)
-
-    # count_vect = CountVectorizer(min_df=0, ngram_range=(1, 2))
-    # genre_mat = count_vect.fit_transform(list(df["특징"]) + [sum_find])
-    # genre_sim = cosine_similarity(genre_mat, genre_mat)
-    # genre_sim_sorted_ind = genre_sim.argsort()[:, ::-1]
+    count_vect = CountVectorizer(min_df=0, ngram_range=(1, 2))
+    genre_mat = count_vect.fit_transform(list(df["특징"]) + [sum_find])
+    genre_sim = cosine_similarity(genre_mat, genre_mat)
+    genre_sim_sorted_ind = genre_sim.argsort()[:, ::-1]
     
-    # last_index = len(df)
-    # similar_indexes = genre_sim_sorted_ind[last_index, :(top_n * 2)]
-    # similar_indexes = similar_indexes.reshape(-1)
-    # similar_indexes = similar_indexes[similar_indexes != last_index]  # 타이틀 기준 제거
-    # for i in title_indexes:
-    #     similar_indexes = similar_indexes[similar_indexes != i]  # 타이틀 기준 제거
-    # similar_indexes
+    last_index = len(df)
+    similar_indexes = genre_sim_sorted_ind[last_index, :(top_n * 2)]
+    similar_indexes = similar_indexes.reshape(-1)
+    similar_indexes = similar_indexes[similar_indexes != last_index]  # 타이틀 기준 제거
+    for i in title_indexes:
+        similar_indexes = similar_indexes[similar_indexes != i]  # 타이틀 기준 제거
+    similar_indexes
     
-    # return df.iloc[similar_indexes].sort_values("가중치평점", ascending=False)[:top_n]
+    return df.iloc[similar_indexes].sort_values("가중치평점", ascending=False)[:top_n]
