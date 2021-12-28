a015fd23b51eb18ae4c6fa2e44d48220d5559fd8
[3기-2반] 이무원 <tlsehd482@gmail.com>
2021-12-28 17:54:52 +0900

    feat: 유저 평가결과 DB에 저장기능 추가

--- back/api/contents.py
+++ back/api/contents.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, Blueprint, request, session
-# from back.models import Buy, Streaming, Rent
-from models import User, Contents, Genre, Actor, Buy, Streaming, Rent
+from numpy.random.mtrand import randint
+from scipy.sparse.construct import random
+from models import User, Contents, Genre, Actor, Buy, Streaming, Rent, User_Taste
 from db_connect import db
 from datateam.Recommendation import recommendations
 
@@ -28,9 +29,24 @@ def recommend():
     if request.method == 'POST':
         params = request.get_json()
         user_pick_list = params['data']
-        user_pick_id = [i[0] for i in user_pick_list]
+        user_pick_id = [i['contents_id'] for i in user_pick_list]
+        user_scores = [i['score'] for i in user_pick_list]
+        if session.get('email'):
+            user_email = session['email']
+            user_id = User.query.filter(User.email == user_email).first()['id']
+            for taste in user_pick_list:
+                new_taste = User_Taste(
+                    user_id = user_id,
+                    contents_id = taste['contents_id'], 
+                    score = taste['score'] if 'score' in taste else None,
+                    is_picked = taste['is_picked'] if 'is_picked' in taste else False
+                )
+                db.session.add(new_taste)
+            db.session.commit()
+
     else:
-        user_pick_id = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
+        user_pick_id = random.sample([i for i in range(1, 100)], 10)
+        user_scores = [randint(1, 10) for _ in range(10)]
     con_list = Contents.query.filter(Contents.id.in_(user_pick_id))
     user_pick = [i.title for i in con_list]
 
