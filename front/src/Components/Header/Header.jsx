03b79611d58373af415575a9f29af890bc258b35
hyunhaha <gusgk2658@naver.com>
2021-12-30 00:58:52 +0900

    feat: 로그인시 header 변경

--- front/src/Components/Header/Header.jsx
+++ front/src/Components/Header/Header.jsx
@@ -1,21 +1,49 @@
+import axios from "axios";
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Header = props => {
+  const [isLogin, setIsLogin] = useState(false);
+  useEffect(() => {
+    console.log(isLogin);
+  }, [isLogin]);
+  useEffect(() => {
+    axios.get("/api/user/isSignin").then(res => {
+      console.log(res);
+      if (res.data.status === 200) {
+        setIsLogin(true);
+      }
+    });
+  });
+  const logout = () => {
+    axios.get("api/user/signout").then(res => {
+      console.log(res);
+      if (res.data.status === 200) {
+        setIsLogin(false);
+      }
+    });
+  };
   return (
     <HeaderWrap>
       <Title>
         <Link to="/">Black Pizza 🍕</Link>
       </Title>
-      <Nav>
-        <LinkBlock>
-          <Link to="/signup">가입하기</Link>
-        </LinkBlock>
-        <LinkBlock>
-          <Link to="/login">로그인</Link>
-        </LinkBlock>
-      </Nav>
+
+      {isLogin === true ? (
+        <button onClick={logout}>로그아웃</button>
+      ) : (
+        <Nav>
+          <LinkBlock>
+            <Link to="/signup">가입하기</Link>
+          </LinkBlock>
+          <LinkBlock>
+            <Link to="/login">로그인</Link>
+          </LinkBlock>
+        </Nav>
+      )}
     </HeaderWrap>
   );
 };
