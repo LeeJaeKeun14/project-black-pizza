af54483234720707a91eeaba11857092e52f785c
hyunhaha <gusgk2658@naver.com>
2022-01-07 03:46:34 +0900

    feat: 로딩 스피너 추가

--- front/src/Pages/SurveyResult/SurveyResult.jsx
+++ front/src/Pages/SurveyResult/SurveyResult.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import ResultChart from "../../Components/Chart/ResultChart";
-
 import Header from "../../Components/Header/Header";
 import { useResult } from "../../hooks/useResult";
 import ChartDesc from "./ChartDesc";
@@ -9,23 +7,18 @@ import Item from "./Item";
 
 const SurveyResult = props => {
   const surveyResult = useResult();
+
   return (
     <SurveyResultBlock>
       <Header />
       <Title>추천 결과</Title>
       {surveyResult.isLoading ? (
-        <div>loading...</div>
+        <LoadingWrap>
+          <img src="/images/pizzaLoading.gif" alt="loading" loading="lazy" />
+        </LoadingWrap>
       ) : (
         <div>
           <ChartDesc ottData={surveyResult.data[1]} />
-          {/* <ChartBlock>
-            <ResultChart data={surveyResult.data[1]} />
-            <div>
-              <div>회원님에게 추천하는 OTT 서비스는 00입니다.</div>
-              <div></div>
-            </div>
-          </ChartBlock> */}
-
           <Content>
             {Object.entries(surveyResult.data[0]).map(([key, list], idx) => (
               <Item key={idx} id={key} data={list} />
@@ -43,9 +36,9 @@ const SurveyResultBlock = styled.div`
 const Title = styled.h2`
   text-align: center;
 `;
-const ChartBlock = styled.section`
-  display: flex;
-  justify-content: space-around;
+const LoadingWrap = styled.div`
+  text-align: center;
+  padding-top: 50px;
 `;
 const Content = styled.section`
   padding: 50px;
