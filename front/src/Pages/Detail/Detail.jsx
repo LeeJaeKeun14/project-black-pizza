8b665ac8273c0ca885c5de8d67b9d8d0b402e574
hyunhaha <gusgk2658@naver.com>
2022-01-07 03:50:36 +0900

    feat: 디테일 페이지 찜하기 기능 추가

--- front/src/Pages/Detail/Detail.jsx
+++ front/src/Pages/Detail/Detail.jsx
@@ -1,59 +1,89 @@
-import axios from "axios";
-import React, { useEffect, useMemo, useState } from "react";
-import { useQuery } from "react-query";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import Header from "../../Components/Header/Header";
 import { useContentDetail } from "../../hooks/useContent";
+import { useUserPickPost } from "../../hooks/useUserPick";
 
 const Detail = props => {
   const { id } = useParams();
-  const contentDetail = useContentDetail(id);
+  const { data, isLoading } = useContentDetail(parseInt(id));
+  const [isPicked, setIsPicked] = useState(false);
+  const userPickPost = useUserPickPost();
 
-  const runingtime = inputTime => {
+  useEffect(() => {
+    if (data) setIsPicked(data.is_picked);
+  }, [data]);
+
+  useEffect(() => {
+    if (userPickPost.isSuccess) {
+      setIsPicked(cur => !cur);
+    }
+  }, [userPickPost.isSuccess]);
+
+  const runningTime = inputTime => {
     return inputTime
       .split(":")
       .map((e, i) => (i === 0 ? `${parseInt(e)}시간` : `${parseInt(e)}분`))
       .join(" ");
   };
+  const postUserPick = () => {
+    userPickPost.mutate([{ contents_id: parseInt(id), is_picked: true }]);
+  };
+
+  const cancelUserPick = () => {
+    userPickPost.mutate([{ contents_id: parseInt(id), is_picked: false }]);
+  };
+
   return (
     <DetailBlock>
       <Header />
-      {contentDetail.isLoading ? (
+      {isLoading ? (
         <div>loading...</div>
       ) : (
         <InfoBlock>
-          <DetailTitle>{`${contentDetail.data.title} 상세정보`}</DetailTitle>
+          <DetailTitle>{`${data.title} 상세정보`}</DetailTitle>
           <Info>
-            <img src={contentDetail.data.image} alt="poster" />
-            <div>
-              <Title>{contentDetail.data.title}</Title>
+            <img src={data.image} alt="poster" />
+            <InfoTextPart>
+              <Title>{data.title}</Title>
               <YearGenreWrap>
-                <Year>{contentDetail.data.open_year}</Year>
+                <Year>{data.open_year}</Year>
                 <div>
-                  {contentDetail.data.genre.map((e, i) => (
+                  {data.genre.map((e, i) => (
                     <GenreTag key={i}>{e.trim()}</GenreTag>
                   ))}
                 </div>
               </YearGenreWrap>
 
-              <div>{runingtime(contentDetail.data.runtime)}</div>
+              <div>{runningTime(data.runtime)}</div>
               <div>
                 <SpanTitle>감독 </SpanTitle>
-                <span> {contentDetail.data.director}</span>
+                <span> {data.director}</span>
               </div>
               <div>
                 <SpanTitle>배우 </SpanTitle>
-                {contentDetail.data.actor.map((e, i) => (
+                {data.actor.map((e, i) => (
                   <span key={i}>
-                    {i === contentDetail.data.actor.length - 1
-                      ? `${e}`
-                      : `${e} ・`}
+                    {i === data.actor.length - 1 ? `${e}` : `${e} ・`}
                   </span>
                 ))}
               </div>
-            </div>
+              {isPicked ? (
+                <Button onClick={cancelUserPick} isPicked={isPicked}>
+                  찜 취소
+                </Button>
+              ) : (
+                <Button onClick={postUserPick} isPicked={isPicked}>
+                  찜하기
+                </Button>
+              )}
+            </InfoTextPart>
           </Info>
+          <SynopsisPart>
+            <SynopsisTitle>시놉시스</SynopsisTitle>
+            <p>{data.synopsis}</p>
+          </SynopsisPart>
         </InfoBlock>
       )}
     </DetailBlock>
@@ -74,11 +104,15 @@ const DetailTitle = styled.h1`
 `;
 const Info = styled.div`
   display: flex;
-  justify-content: space-evenly;
+  justify-content: space-between;
+  padding: 50px;
   align-items: center;
   div + div {
     margin-top: 10px;
   }
+`;
+const InfoTextPart = styled.div`
+  padding-left: 50px;
 `;
 const Title = styled.h2`
   ${({ theme }) => theme.font.large}
@@ -100,5 +134,25 @@ const GenreTag = styled.span`
 `;
 const SpanTitle = styled.span`
   font-weight: bold;
+`;
+const SynopsisPart = styled.div`
+  padding: 50px;
+`;
+const SynopsisTitle = styled.div`
+  font-weight: bold;
+`;
+const Button = styled.button`
+  background-color: ${props =>
+    props.isPicked ? ({ theme }) => theme.color.coral : "#ffffff00"};
+  border: ${props => (props.isPicked ? "none" : "1px solid white")};
+  padding: 8px 16px;
+  border-radius: 15px;
+  cursor: pointer;
+  color: ${({ theme }) => theme.color.font};
+  margin-top: 10px;
+  &:hover {
+    background-color: ${({ theme }) => theme.color.coral};
+    border-color: runningTime ${({ theme }) => theme.color.coral};
+  }
 `;
 export default Detail;
