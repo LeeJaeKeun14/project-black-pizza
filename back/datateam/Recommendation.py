9d2e9f271de91a114abf064a158a3d2a6971d580
LeeJaeKeun14 <ljkean14@gmail.com>
2022-01-07 15:15:32 +0900

    fix: 랜덤 함수, 비복원 추출로 변환

--- back/datateam/Recommendation.py
+++ back/datateam/Recommendation.py
@@ -46,8 +46,8 @@ def recommendations(user_pick, contents_all, genre_matrix):
     third_recommends = list(result.sort_values("가중치평점", ascending=False)[6:].index)
     
     total = []
-    total += rd.choices(first_recommends, k=9)
+    total += np.random.choice(first_recommends, 9, replace=False)
     total += second_recommends
-    total += rd.choices(third_recommends, k=5)
+    total += np.random.choice(first_recommends, 5, replace=False)
 
     return list(df.iloc[total]["id"])
